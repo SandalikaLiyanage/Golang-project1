135ea78ef8f31d155da31b21d5dc8801212b1fcf
SandalikaLiyanage <sandalika24@gmail.com>
2024-04-23 15:11:04 +0530

    go quiz project first commit

---
+++ tutorial.go
@@ -0,0 +1,50 @@
+package main //important
+import "fmt" //import a library so that we can be able to output stuff to the console
+
+//project- quiz game
+func main(){
+	fmt.Println("Welcome to my quiz game!")
+	//<<data types>>uint-unsigned integer-anypositive whole number,float64-we use 64 bits of our computer memory to store this float
+	//name:="Tim"->this is like ,var name string="Tim"
+	//fmt.Printf("Hello %v", name), here this is a print function , it will take an argument and assing it to the value(%v)
+	
+	// let's now take user inputs
+	fmt.Printf("Enter your name: ")
+	var name string
+	fmt.Scan(&name)//&-go to the memory adress location of tis variable, scan requires th memory loction to assign the value to the variable
+	fmt.Printf("Hello, %v , welcome to the game!\n",name)
+	fmt.Printf("Enter your age: ")
+	var age uint
+	fmt.Scan(&age)
+
+	if age >=10{
+		fmt.Println("Yay you can play!")
+	}else{
+		fmt.Println("You cannot play!")
+		return //break out of this function
+	}
+	fmt.Printf("What is better ,the RTX 3000 or RTX 3090? ")
+	var answer string
+	var answer2 string//to get user inputs seperated by space. store them in 2 seperate variables
+	fmt.Scan(&answer, &answer2)
+	//check the answer is correct or not
+	if(answer + " " + answer2=="RTX 3090"){
+		fmt.Println("Correct!")
+	}else if(answer + " " + answer2=="rtx 3090"){
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("Incorrect!")
+	}
+
+	fmt.Printf("How many cores does the Ryzen 9 3900x have? ")
+	var cores int
+	fmt.Scan(&cores)
+
+	if cores==12{
+		fmt.Println("Correct!")
+	}else{
+		fmt.Println("Incorrect!")
+	}
+
+}
+
